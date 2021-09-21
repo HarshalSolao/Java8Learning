f558c9f86487ec242a4c547251ddfdfbbe4476d7
Harshal Solao <harshalsolao@gmail.com>
2021-09-21 11:50:48 +0530

    To check

--- src/main/java/com/demo/threading/ProducerConsumerExampleWithWaitAndNotify.java
+++ src/main/java/com/demo/threading/ProducerConsumerExampleWithWaitAndNotify.java
@@ -7,6 +7,7 @@ public class ProducerConsumerExampleWithWaitAndNotify
 {
    public static void main(String[] args)
    {
+      //
       List<Integer> taskQueue = new ArrayList<Integer>();
       int MAX_CAPACITY = 5;
       Thread tProducer = new Thread(new Producer(taskQueue, MAX_CAPACITY), "Producer");
